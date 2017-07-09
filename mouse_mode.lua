06f6c3a038fbcaccc049c3926795ff74b9a55c84
Krittanon.w <krittanon.w@gmail.com>
2017-07-09 14:51:42 +0700

    refact mouse move to triger scteen for usage alot of variable

--- mouse_mode.lua
+++ mouse_mode.lua
@@ -1,6 +1,5 @@
 
 local function click()
-    -- local cmd = "cliclick c:+0,+0"
     local cmd = "/usr/local/bin/cliclick c:+0,+0"
     local printout = hs.execute(cmd)
     return printout
@@ -13,22 +12,20 @@ mouse_positon_before = {
 
 move_event = move_event or hs.eventtap.new({ hs.eventtap.event.types.mouseMoved },
     function(event)
-            local dx = event:getProperty(hs.eventtap.event.properties.mouseEventDeltaX)
-            local dy = event:getProperty(hs.eventtap.event.properties.mouseEventDeltaY)
-            local current_mouse_position = hs.mouse.getRelativePosition()
+        local current_mouse_position = hs.mouse.getRelativePosition()
 
-            if (math.abs(mouse_positon_before.x - current_mouse_position.x) > 500) then
-                hs.printf('Screen changed')
-                hs.timer.doAfter(0.15,
-                    function()
-                        click()
-                    end
-                )
-                -- hs.osascript.applescript('display notification "hello" & return')
-            end
-            -- hs.printf('x ' .. current_mouse_position.x ..', y ' .. current_mouse_position.y)
-            mouse_positon_before.x = current_mouse_position.x
-            mouse_positon_before.y = current_mouse_position.y
+        if (math.abs(mouse_positon_before.x - current_mouse_position.x) > 500) then
+            -- hs.printf('Screen changed')
+            hs.timer.doAfter(0.05,
+                function()
+                    click()
+                end
+            )
+            -- hs.osascript.applescript('display notification "hello" & return')
+        end
+        -- hs.printf('x ' .. current_mouse_position.x ..', y ' .. current_mouse_position.y)
+        mouse_positon_before.x = current_mouse_position.x
+        mouse_positon_before.y = current_mouse_position.y
     end
 )
 
