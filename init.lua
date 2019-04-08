970f784523415e46453a7241c8d2edbce4587d06
krittanon-w <krittanon.w@gmail.com>
2019-04-09 02:17:06 +0700

    add windows hints

--- init.lua
+++ init.lua
@@ -49,7 +49,7 @@ if spoon.Resize then
 
     hsResizeMode_keys = hsResizeMode_keys or {"alt", "R"}
     if string.len(hsResizeMode_keys[2]) > 0 then
-        spoon.ModalMan.supervisor:bind(hsResizeMode_keys[1], hsResizeMode_keys[2], "Enter ResizeMode Environment", function()
+        spoon.ModalMan.supervisor:bind(hsResizeMode_keys[1], getKeyCode(hsResizeMode_keys[2]), "Enter ResizeMode Environment", function()
             spoon.ModalMan:deactivateAll()
             spoon.ModalMan:activate({"ResizeMode"})
         end)
@@ -61,32 +61,32 @@ end
 if spoon.Vim then
     spoon.ModalMan:new("VimMode", "#000000")
     local cmodal = spoon.ModalMan.modal_list["VimMode"]
-    local modsA = {"alt"}
-    local modsB = {"alt", "shift"}
     -- cmodal:bind("", "escape", "Deactivate VimMode", function() spoon.ModalMan:deactivate({"VimMode"}) end)
     -- cmodal:bind("", "Q", "Deactivate VimMode", function() spoon.ModalMan:deactivate({"VimMode"}) end)
     -- cmodal:bind("", "tab", "Toggle Cheatsheet", function() spoon.ModalMan:toggleCheatsheet() end)
 
-    cmodal:bind(modsA, getKeyCode("H"), function() spoon.Vim:move("left") end, nil, function() spoon.Vim:move("left") end)
-    cmodal:bind(modsA, getKeyCode("L"), function() spoon.Vim:move("right") end, nil, function() spoon.Vim:move("right") end)
-    cmodal:bind(modsA, getKeyCode("J"), function() spoon.Vim:move("down") end, nil, function() spoon.Vim:move("down") end)
-    cmodal:bind(modsA, getKeyCode("K"), function() spoon.Vim:move("up") end, nil, function() spoon.Vim:move("up") end)
+    cmodal:bind({"alt"}, getKeyCode("H"), function() spoon.Vim:move("left") end, nil, function() spoon.Vim:move("left") end)
+    cmodal:bind({"alt"}, getKeyCode("L"), function() spoon.Vim:move("right") end, nil, function() spoon.Vim:move("right") end)
+    cmodal:bind({"alt"}, getKeyCode("J"), function() spoon.Vim:move("down") end, nil, function() spoon.Vim:move("down") end)
+    cmodal:bind({"alt"}, getKeyCode("K"), function() spoon.Vim:move("up") end, nil, function() spoon.Vim:move("up") end)
     
-    cmodal:bind(modsA, getKeyCode("I"), function() spoon.Vim:delete("left") end, nil, function() spoon.Vim:delete("left") end)
-    cmodal:bind(modsA, getKeyCode("O"), function() spoon.Vim:delete("right") end, nil, function() spoon.Vim:delete("right") end)
+    cmodal:bind({"alt"}, getKeyCode("I"), function() spoon.Vim:delete("left") end, nil, function() spoon.Vim:delete("left") end)
+    cmodal:bind({"alt"}, getKeyCode("O"), function() spoon.Vim:delete("right") end, nil, function() spoon.Vim:delete("right") end)
     
-    cmodal:bind(modsB, getKeyCode("I"), function() spoon.Vim:delete("left_word") end, nil, function() spoon.Vim:delete("left_word") end)
-    cmodal:bind(modsB, getKeyCode("O"), function() spoon.Vim:delete("right_word") end, nil, function() spoon.Vim:delete("right_word") end)
+    cmodal:bind({"alt", "shift"}, getKeyCode("I"), function() spoon.Vim:delete("left_word") end, nil, function() spoon.Vim:delete("left_word") end)
+    cmodal:bind({"alt", "shift"}, getKeyCode("O"), function() spoon.Vim:delete("right_word") end, nil, function() spoon.Vim:delete("right_word") end)
 
-    cmodal:bind(modsA, getKeyCode("Y"), function() spoon.Vim:jump("start") end, nil, function() spoon.Vim:jump("start") end)
-    cmodal:bind(modsA, getKeyCode("U"), function() spoon.Vim:jump("end") end, nil, function() spoon.Vim:jump("end") end)
+    cmodal:bind({"alt"}, getKeyCode("Y"), function() spoon.Vim:jump("start") end, nil, function() spoon.Vim:jump("start") end)
+    cmodal:bind({"alt"}, getKeyCode("U"), function() spoon.Vim:jump("end") end, nil, function() spoon.Vim:jump("end") end)
     
-    cmodal:bind(modsB, getKeyCode("H"), function() spoon.Vim:jump("left_word") end, nil, function() spoon.Vim:jump("left_word") end)
-    cmodal:bind(modsB, getKeyCode("L"), function() spoon.Vim:jump("right_word") end, nil, function() spoon.Vim:jump("right_word") end)
+    cmodal:bind({"alt", "shift"}, getKeyCode("H"), function() spoon.Vim:jump("left_word") end, nil, function() spoon.Vim:jump("left_word") end)
+    cmodal:bind({"alt", "shift"}, getKeyCode("L"), function() spoon.Vim:jump("right_word") end, nil, function() spoon.Vim:jump("right_word") end)
+    
+    cmodal:bind({"cmd"}, getKeyCode("M"), function() hs.hints.windowHints() end)
 
     hsVimMode_keys = hsVimMode_keys or {"alt", "V"}
     if string.len(hsVimMode_keys[2]) > 0 then
-        spoon.ModalMan.supervisor:bind(hsVimMode_keys[1], hsVimMode_keys[2], "Enter VimMode Environment", function()
+        spoon.ModalMan.supervisor:bind(hsVimMode_keys[1], getKeyCode(hsVimMode_keys[2]), "Enter VimMode Environment", function()
             spoon.ModalMan:deactivateAll()
             spoon.ModalMan:activate({"VimMode"})
         end)
@@ -98,3 +98,5 @@ end
 
 spoon.ModalMan:getInstance():enter()
 hs.alert("- HammerSpoon -")
+-- hs.hints.windowHints()
+
